f342206f203d0816346cc23afb386151acf72343
Mavlon4658 <mavlonbe4658>
2024-08-11 19:59:19 +0500

    project is ready

--- js/main.js
+++ js/main.js
@@ -1,47 +1,52 @@
-let swp = new Swiper('.main-modal .swiper', {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    // initialSlide: 1,
-    pagination: {
-        el: ".swp_navigation",
-        type: "custom",
-        renderCustom: function (swiper, current, total) {
-            let num1 = current < 10 ? `0${current}` : current;
-            let num2 = total < 10 ? `0${total}` : total;
-            return num1 + '/' + num2; 
-        }
-    },
+let swp1 = new Swiper('.swp-1', {
+    slidesPerview: 1,
+    spaceBetween: 0,
+    allowTouchMove: false,
+    effect: 'fade',
+    loop: true,
 })
 
-let nextBtn = document.querySelectorAll('.btn_right'),
-    prevBtn = document.querySelectorAll('.btn_left');
+let swp2 = new Swiper('.swp-2', {
+    slidesPerview: 1,
+    spaceBetween: 0,
+    loop: true,
+    allowTouchMove: false,
+})
 
-nextBtn[0].onclick = () => {
-    swp.slideTo(1);
-}
+let swp3 = new Swiper('.swp-3', {
+    slidesPerview: 1,
+    spaceBetween: 0,
+    allowTouchMove: false,
+    loop: true,
+    effect: 'fade',
+})
 
-prevBtn[1].onclick = () => {
-    swp.slideTo(0);
-}
+let mainBtnPrev = document.querySelectorAll('.main_swp .swp_btns button')[0],
+    mainBtnNext = document.querySelectorAll('.main_swp .swp_btns button')[1],
+    mainBtnNext2 = document.querySelectorAll('.main_swp .hand_wrap button'),
+    mainSwpText = document.querySelectorAll('.main_swp .swp_navigation p')[0],
+    mainSwpLength = document.querySelectorAll('.main_swp .swp_navigation p')[1];
 
-let modal = document.querySelector('.main-modal'),
-    modalOpen = document.querySelectorAll('.main-modal__open'),
-    modalClose = document.querySelector('.main-modal_head__btn');
-
-if (modal) {
-    modalOpen.forEach(el => {
-        el.onclick = e => {
-            e.preventDefault();
-            modal.classList.add('active');
+if (mainBtnNext) {
+    mainSwpLength.textContent = swp2.slides.length;
+    mainBtnNext.onclick = () => {
+        swp1.slideNext();
+        swp2.slideNext();
+        swp3.slideNext();
+        mainSwpText.textContent = swp2.realIndex + 1;
+    }
+    mainBtnPrev.onclick = () => {
+        swp1.slidePrev();
+        swp2.slidePrev();
+        swp3.slidePrev();
+        mainSwpText.textContent = swp2.realIndex + 1;
+    }
+    mainBtnNext2.forEach(el => {
+        el.onclick = () => {
+            swp1.slideNext();
+            swp2.slideNext();
+            swp3.slideNext();
+            mainSwpText.textContent = swp2.realIndex + 1;
         }
     })
-
-    modalClose.onclick = e => {
-        e.preventDefault();
-        modal.classList.remove('active');
-        modal.classList.add('end-active');
-        setTimeout(() => {
-            modal.classList.remove('end-active');
-        }, 300);
-    }
 }
